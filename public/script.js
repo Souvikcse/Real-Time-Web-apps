1711fd2131875acc62a65aaa2c2291e14e7175c3
Souvikcse <sayanwinz@gmail.com>
2019-09-07 23:46:39 +0530

    chat app
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

--- public/script.js
+++ public/script.js
@@ -4,14 +4,20 @@ socket.on('connected', () => {
 })
 
 $(function(){
+    let user = '';
     $('#btn-send').click(function () { 
         let msg = $('#msgbox').val();
-        socket.emit('msg', {message: msg})
+        socket.emit('msg', {message: msg, user: user})
+    })
+    $('#login-btn').click(function () {
+        user = $('#login-id').val();
+            $('#chat-box').show();
+            $('#login-box').hide();
     })
 
         socket.on('recev_msg', (data) => {
             $('#msglist').append(
-                $('<li>' + data.message + '</li>')
+                $('<li>' + data.user + ': ' + data.message + '</li>')
             )
     })
 })
